b578710fa82a7c125ab28a234532e7bdcdf2b4c1
Jere Mäkinen <jere.makinen@tuni.fi>
2020-11-17 18:53:06 +0200

    Ufo-luokkaan lisätty yksinkertaisimpien metodien toteutuksia

--- Game/ufo.cpp
+++ Game/ufo.cpp
@@ -1,8 +1,9 @@
 #include "ufo.hh"
 
-Ufo::Ufo()
+Ufo::Ufo() :
+    speed_(0),
+    removed_(false)
 {
-
 }
 
 Ufo::~Ufo()
@@ -12,25 +13,25 @@ Ufo::~Ufo()
 
 Interface::Location Ufo::giveLocation() const
 {
-
+    return location_;
 }
 
 void Ufo::move(Interface::Location loc)
 {
-
+    location_ = loc;
 }
 
 bool Ufo::isRemoved() const
 {
-
+    return removed_;
 }
 
 void Ufo::remove()
 {
-
+    removed_ = true;
 }
 
 void Ufo::setSpeed(int v)
 {
-
+    speed_ = v;
 }
